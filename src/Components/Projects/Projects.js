21aa3c5f678d7842d701df175f069ba44a57dbdc
solayman1e <solaymanweb27@gmail.comm>
2022-06-27 10:29:37 +0600

    Heloo

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -64,7 +64,7 @@ const Projects = () => {
                                             <Card.Body className='card-details'>
                                                 <Card.Title>Title: {site.name}</Card.Title>
                                                 <Card.Text>
-                                                    Some quick example text to build on the card title and make up the bulk of 
+                                                    Some quick example text to build on the card title and make up the bulk of love love love
                                                     the card's content.
                                                 </Card.Text>
                                                 <div className='d-flex'>
