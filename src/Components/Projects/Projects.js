d0dc73b2074536ee70d534c6f3ca7c6d9ff1f5ea
solayman1e <solaymanweb27@gmail.comm>
2022-06-27 18:50:42 +0600

    project image update

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,44 +1,52 @@
 import React from 'react';
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
-import electricTools from '../../Assest/electricTools.png';
-import fridgeHouse from '../../Assest/friedgeHouse.png';
-import gymSite from '../../Assest/gymeWebSite.png';
-import nikeReact from '../../Assest/nikeReact.png';
-import convarsion from '../../Assest/ConventionalSite.png'
+
+import img1 from '../../Assest/p-1.png'
+import img2 from '../../Assest/p-2.png'
+import img3 from '../../Assest/p-3.png'
+import img4 from '../../Assest/p-4.png'
+import img5 from '../../Assest/p-5.png'
+import img6 from '../../Assest/p-6.png'
 import './Projects.css';
 
 const Projects = () => {
 
     const allDemoSite = [
         {
-            "image": electricTools,
-            "name": "Menufectural web site",
+            "image": img1,
+            "name": "PSD to HTML.....",
+            "technology":"",
             "live": "https://solaymanweb.github.io/themezone/",
             "discription": "When you have Walton refrigerator & freezer in your home",
         },
         {
-            "image": fridgeHouse,
-            "name": "Managemet web site",
+            "image": img2,
+            "name": "Managemet web site with ReactJS, nodeJS",
+            "live":"https://electric-tools.netlify.app/",
             "discription": "you are safe from going to market every day that keeps",
         },
         {
-            "image": electricTools,
-            "name": "Manufectural web site",
+            "image": img3,
+            "name": "Manufectural web site with ReactJS, nodeJS",
+            "live":"https://fridge-store-house.netlify.app/",
             "discription": "you free from extra burden. You can keep all your foods and stuffs",
         },
         {
-            "image": gymSite,
-            "name": "gyme orgaingetion ",
+            "image": img4,
+            "name": "Figma to HTML ",
+            "live": "https://solaymanweb.github.io/Figma-to-html/",
             "discription": "Our energy-saving, stylish, equipped with 100%",
         },
         {
-            "image":nikeReact,
-            "name": "basic e-commerce",
+            "image":img5,
+            "name": "PSD to HTML",
+            "live": "https://solaymanweb.github.io/lucky-one/",
             "discription": "Walton refrigerator & freezer will make a vivid distinction in your kitchen",
         },
         {
-            "image": convarsion,
-            "name": "Conversion site",
+            "image": img6,
+            "name": "PSD to HTML___",
+            "live":"https://solaymanweb.github.io/PSD-to-HTML/",
             "discription": "WFA-2A3-GDXX-XX ",
 
         }
@@ -62,15 +70,13 @@ const Projects = () => {
                                                 <Card.Img variant="top" src={site.image} />
                                             </div>
                                             <Card.Body className='card-details'>
-                                                <Card.Title>Title: {site.name}</Card.Title>
+                                                <Card.Title> {site.name}</Card.Title>
                                                 <Card.Text>
-                                                    Some quick example text to build on the card title and make up the bulk of
-
-                                                    the card's content.
+                                                    {site.technology}
                                                 </Card.Text>
                                                 <div className='d-flex'>
-                               <a href={site.live} target="_blank" rel="noopener noreferrer"> <button className="button-87" role="button">Live</button></a>
-                                <button className="button-87" role="button">Github</button>
+                               <a href={site.live} target="_blank" rel="noopener noreferrer"> <Button className="button-87 live-btn" role="button">view live site</Button></a>
+
                                 
                                                 </div>
                                             </Card.Body>
