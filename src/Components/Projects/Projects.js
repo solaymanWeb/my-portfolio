61057d557f38dcbc95f7d5b50cb063540e52fe35
solayman1e <solaymanweb27@gmail.comm>
2022-06-27 14:55:49 +0600

    footer

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -64,7 +64,7 @@ const Projects = () => {
                                             <Card.Body className='card-details'>
                                                 <Card.Title>Title: {site.name}</Card.Title>
                                                 <Card.Text>
-                                                    Some quick example text to build on the card title and make up the bulk of Love 
+                                                    Some quick example text to build on the card title and make up the bulk of
 
                                                     the card's content.
                                                 </Card.Text>
