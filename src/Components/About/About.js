eb1644b34005702aad4571ddd706b00a237e9c08
solayman1e <solaymanweb27@gmail.comm>
2022-07-03 21:06:58 +0600

    compleated project

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -55,12 +55,11 @@ const About = () => {
 
     return (
         <section id='aboutMe' className='about-section' >
-            <h2 data-aos="zoom-in-left" className='fw-bold py-4'>About <span className='text-orangered'>Me</span></h2>
+            <h2  data-aos="zoom-in-left" className='fw-bold py-5 '>About <span className='text-orangered'>Me</span></h2>
             <Container className='px-5'>
                 <Row className='g-5' >
                     <Col md={6}>
-                        <div data-aos="zoom-in" className='my-self-container'>
-                        <h2 className='text-upparcase py-3 fw-bold'>About <span className='text-orangered'>MySelf</span></h2>
+                        <div data-aos="zoom-in" className='my-self-container '>
                         <div className='my-self'>
                         <p className='fs-5 text-start '>I am a Front-end web Developer. I am working as a Web Developer since 2019. I have experience with some projects for web design and development. I can provide clean code and pixel-perfect Design. I work with HTML5, CSS3, Bootstrap, Tailwind,  React, Node.js, Express.js, MongoDB, and Responsive Design.</p>
                         </div>
@@ -72,7 +71,7 @@ const About = () => {
                             {
                                 skillsImage.map(skill =>
                                 <Col md={3}>
-                                    <div className='skill-image '>
+                                    <div data-aos="zoom-in" className='skill-image '>
                                     <img src={skill.image} alt="" />
                                     <h5 className='fs-bold'>{skill.name}</h5>
                                     </div>
